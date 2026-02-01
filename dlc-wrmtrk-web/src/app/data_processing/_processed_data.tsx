a26e5d8a97b58424a61847bcd348fee8e565b40a
Tyllis Xu <LivelyCarpet87@gmail.com>
2026-02-01 16:59:35 -0600

    display error messages

--- dlc-wrmtrk-web/src/app/data_processing/_processed_data.tsx
+++ dlc-wrmtrk-web/src/app/data_processing/_processed_data.tsx
@@ -24,7 +24,7 @@ const VideoTile = memo(function VideoTile({md5}:{md5:string}){
     
     const proc_state = videoSWR.data?.proc_state;
     useEffect(() => {
-        if (proc_state === "done" || proc_state === "warning" || proc_state === "error" || proc_state === "failed") {
+        if (proc_state === "done" || proc_state.includes("warning") || proc_state.includes("failed")) {
             pauseRef.current = true;
         }
     }, [proc_state]);
@@ -55,25 +55,17 @@ const VideoTile = memo(function VideoTile({md5}:{md5:string}){
                 </svg>
             </Tooltip>
         );
-    } else if (proc_state == "warning"){
+    } else if (proc_state.includes("warning")){
         proc_indicator = (
-            <Tooltip label="Slight inconsistencies detected during processing." openDelay={700}>
+            <Tooltip label={proc_state} openDelay={300}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-blue-800">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m0-10.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.25-8.25-3.286Zm0 13.036h.008v.008H12v-.008Z" />
                 </svg>
             </Tooltip>
         );
-    } else if (proc_state == "error"){
+    } else if (proc_state.includes("failed")){
         proc_indicator = (
-            <Tooltip label="Extreme inconsistencies created during processing." openDelay={700}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-yellow-700">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
-                </svg>
-            </Tooltip>
-        );
-    } else if (proc_state == "failed"){
-        proc_indicator = (
-            <Tooltip label="Video failed to process." openDelay={700}>
+            <Tooltip label={proc_state} openDelay={300}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-red-800">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                 </svg>
@@ -90,7 +82,7 @@ const VideoTile = memo(function VideoTile({md5}:{md5:string}){
     }
 
     let dataDisplay = <></>;
-    if (proc_state == "done" || proc_state == "warning"){
+    if (proc_state == "done" || proc_state.includes("warning")){
         const tableRows = [] as React.JSX.Element[];
         for (const i in videoSWR.data.displayData.table){
             const row = videoSWR.data.displayData.table[i];
